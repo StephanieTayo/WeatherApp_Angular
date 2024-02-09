3f7e34c0b2d658079e4874690388ccdcb3f4b1d7
Tayocode <84019316+StephanieTayo@users.noreply.github.com>
2024-02-08 19:30:45 -0800

    background styling

---
+++ WeatherApp/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  imports: [CommonModule,
+BrowserModule],
+  declarations: [AppComponent],
+})
+
+
+export class AppModule {}
