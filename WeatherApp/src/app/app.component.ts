987db7c9bc19f9e081a540d28a5ba038f93940a5
Tayocode <84019316+StephanieTayo@users.noreply.github.com>
2024-02-08 22:33:43 -0800

    fixing dumber pipeline error

--- WeatherApp/src/app/app.component.ts
+++ WeatherApp/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [DecimalPipe],
+  
 })
 export class AppComponent {
   title = 'WeatherApp';
